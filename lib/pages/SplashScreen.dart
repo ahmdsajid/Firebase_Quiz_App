36e91bbbc0932dc9ed58d89309260976534884f1
Mahmudul Sajid <155292895+ahmdsajid@users.noreply.github.com>
2025-06-17 20:03:32 +0600

    update

--- lib/pages/SplashScreen.dart
+++ lib/pages/SplashScreen.dart
@@ -1,21 +1,58 @@
+import 'dart:async';
+
+import 'package:firebase_quiz_app/pages/Home.dart';
 import 'package:flutter/material.dart';
 
-class Splashscreen extends StatelessWidget {
+class Splashscreen extends StatefulWidget {
   const Splashscreen({super.key});
 
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
   
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 3), (){
+      Navigator.push(context, MaterialPageRoute(builder: (context)=>MyHome()));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green,
+      backgroundColor: Colors.deepPurpleAccent,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Icon(Icons.quiz,size: 60,),
-            SizedBox(height: 30,),
-            Text("Quiz App",style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold,),),
+            Icon(
+              Icons.quiz_outlined,
+              size: 100,
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              "Quiz App",
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Get ready to test your brain',
+              style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
             const SizedBox(height: 40),
             // Loading Spinner
             const CircularProgressIndicator(
